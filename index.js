44ecd6e539b36857fe3617631c380bc001b86858
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-06-03 11:41:57 +0300

    commit tree 9f6d367415ffe9bfd4eaf0158fc7fd9bbab79d46

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Follow along with the examples here
+/*//function to greet Guadalupe
+function sayHelloToGuadalupe() {
+  console.log("Hello, Guadalupe!");
+}
+sayHelloToGuadalupe();
+
+//function to greet Liz
+function sayHelloToLiz() {
+  console.log("Hello,Liz!");
+}
+sayHelloToLiz();
+
+//function to greet Samip
+function sayHelloToSamip() {
+  console.log("Hello, Samip");
+}
+sayHelloToSamip(); 
+
+//function by passing an argument
+function doSomething(thing) {
+  console.log(thing);
+}
+doSomething("anything");*/
+
+function sayHelloTo(firstName) {
+  console.log(`Hello, $(firstName)!`);
+}
+sayHelloTo("Jane");
+sayHelloTo("Leo");
+sayHelloTo("Emma");
